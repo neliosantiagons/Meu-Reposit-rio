0651662dbbd2778d88a638910cc589374a841875
Nelio Santiago <164454363+neliosantiagons@users.noreply.github.com>
2024-09-30 20:18:19 -0300

    commit pleage

---
+++ script.js
@@ -0,0 +1,29 @@
+
+document.getElementById("conversion-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const fromCurrency = document.getElementById("from-currency").value;
+    const toCurrency = document.getElementById("to-currency").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+
+    const exchangeRates = {
+        BRL: 1,
+        EUR: 0.1621,
+        USD: 0.1801,
+        STN: 4.01
+    };
+
+    const fee = amount * 0.123;
+    const amountAfterFee = amount - fee;
+    const convertedAmount = amountAfterFee / exchangeRates[fromCurrency] * exchangeRates[toCurrency];
+
+    const resultElement = document.getElementById("conversion-result");
+    resultElement.innerHTML = `
+        <p>Quantia original: ${amount} ${fromCurrency}</p>
+        <p>Taxa de envio (7.5%)
+        <p>Valor a Receber = ${convertedAmount.toFixed(2)} ${toCurrency}</p>
+        <p>Para mais informações sobre taxas de câmbio, visite o site do 
+            <a href="https://www.bcb.gov.br/conversao" target="_blank">Banco Central do Brasil</a>.
+        </p>
+    `;
+});
