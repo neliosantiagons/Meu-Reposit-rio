1fd8d54dcdc54b8331e9ae9d2e418884affb8bf3
Nelio Santiago <164454363+neliosantiagons@users.noreply.github.com>
2025-03-27 17:38:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("conversion-form").addEventListener("submit", function(e
 
     const exchangeRates = {
         BRL: 1,
-        EUR: 0.12,
+        EUR: 0.13,
         USD: 0.20,
         STN: 4.1
     };
